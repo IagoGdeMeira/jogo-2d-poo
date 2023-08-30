5abc32757ab0f97508690bdf3a111d11223f3d9b
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-08-30 17:28:04 -0300

    Update Principal.java

--- src/ifpr/paranavai/jogo/principal/Principal.java
+++ src/ifpr/paranavai/jogo/principal/Principal.java
@@ -6,11 +6,14 @@ import ifpr.paranavai.jogo.modelo.FaseUm;
 import javax.swing.JFrame;
 
 public class Principal extends JFrame {
+    public static final int LARGURA_DA_JANELA = 1024;
+    public static final int ALTURA_DA_JANELA = 728;
+    
     public Principal() {
         Fase fase = new FaseUm();
         super.add(fase);
         super.setTitle("Invasores Horizontais");
-        super.setSize(1024, 728);
+        super.setSize(LARGURA_DA_JANELA, ALTURA_DA_JANELA);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.setLocationRelativeTo(null);
         super.setResizable(false);
